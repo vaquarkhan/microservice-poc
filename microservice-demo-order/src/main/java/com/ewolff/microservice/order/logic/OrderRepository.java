4b6ed73e8dcc09813f37e441af2497ddf98b2f55
v.khan@zipcomps.com <vaquar.khan@gmail.com>
2017-01-14 20:46:58 -0600

    commit

--- microservice-demo-order/src/main/java/com/ewolff/microservice/order/logic/OrderRepository.java
+++
@@ -1,9 +0,0 @@
-package com.ewolff.microservice.order.logic;
-
-import org.springframework.data.repository.PagingAndSortingRepository;
-import org.springframework.data.rest.core.annotation.RepositoryRestResource;
-
-@RepositoryRestResource(collectionResourceRel = "order", path = "order")
-interface OrderRepository extends PagingAndSortingRepository<Order, Long> {
-
-}
