8f6a34969d4b62bc462707bb8cfbf53a4c54ba61
Ramazan Sakin <ramazansakin63@gmail.com>
2020-09-26 16:17:55 +0300

    Lombok integration - customer

--- microservice-demo-customer/src/main/java/com/vaquar/microservice/customer/CustomerApp.java
+++ microservice-demo-customer/src/main/java/com/vaquar/microservice/customer/CustomerApp.java
@@ -1,37 +1,33 @@
 package com.vaquar.microservice.customer;
 
-import javax.annotation.PostConstruct;
-
-import org.springframework.beans.factory.annotation.Autowired;
+import lombok.RequiredArgsConstructor;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.stereotype.Component;
 
+import javax.annotation.PostConstruct;
+
 @ComponentScan
 @EnableAutoConfiguration
 @EnableDiscoveryClient
+@RequiredArgsConstructor
 @Component
 public class CustomerApp {
 
-	private final CustomerRepository customerRepository;
+    private final CustomerRepository customerRepository;
 
-	@Autowired
-	public CustomerApp(CustomerRepository customerRepository) {
-		this.customerRepository = customerRepository;
-	}
+    @PostConstruct
+    public void generateTestData() {
+        customerRepository.save(new Customer("Eberhard", "Wolff",
+                "eberhard.wolff@gmail.com", "Unter den Linden", "Berlin"));
+        customerRepository.save(new Customer("Rod", "Johnson",
+                "rod@somewhere.com", "Market Street", "San Francisco"));
+    }
 
-	@PostConstruct
-	public void generateTestData() {
-		customerRepository.save(new Customer("Eberhard", "Wolff",
-				"eberhard.wolff@gmail.com", "Unter den Linden", "Berlin"));
-		customerRepository.save(new Customer("Rod", "Johnson",
-				"rod@somewhere.com", "Market Street", "San Francisco"));
-	}
-
-	public static void main(String[] args) {
-		SpringApplication.run(CustomerApp.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(CustomerApp.class, args);
+    }
 
 }
